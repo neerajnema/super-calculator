cb2fc54c97c6caeb15f145a6ec3d39ff10d17efd
neerajnema <nema.neeraj@yahoo.in>
2016-11-21 19:14:10 -0800

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,9 @@
+function sum() {
+    var sum = 0;
+    if(argumnent && argumnent.length > 0) {
+        for(var i=0; i< argument.length; i++) {
+            sum += argumnent[i];
+        }
+    }
+    return sum;
+}
