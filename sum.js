c5cbaf399fc0eb97d2c26f6e3a6271d02bda32a8
neerajnema <nema.neeraj@yahoo.in>
2016-11-21 19:48:45 -0800

    Update sum.js

--- sum.js
+++ sum.js
@@ -1,8 +1,8 @@
 function sum() {
     var sum = 0;
-    if(argumnent && argumnent.length > 0) {
+    if(argument && argument.length > 0) {
         for(var i=0; i< argument.length; i++) {
-            sum += argumnent[i];
+            sum += argument[i];
         }
     }
     return sum;
